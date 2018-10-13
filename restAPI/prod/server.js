9a041ec827923bd6b33055a3fc7db21c67b2d77c
Adil Altaf <adil@outlook.com>
2018-10-13 08:43:05 -0700

    RestAPI on Heroku

--- restAPI/prod/server.js
+++ restAPI/prod/server.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const PORT = 8000;
+const PORT = process.env.PORT || '8000';
 //this is for running the server
 app_1.default.listen(PORT, () => {
     console.log(`Express server listening on port ${PORT}`);
